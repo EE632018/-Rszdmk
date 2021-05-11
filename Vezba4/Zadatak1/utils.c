2968688ad3ef7f8a68ecab1ee9f740c5b1b57c5c
EE632018 <mstrizak0@gmail.com>
2021-05-11 16:54:36 +0200

    Vezba4 - Doxygen!

---
+++ Vezba4/Zadatak1/utils.c
@@ -0,0 +1,7 @@
+#include "utils.h"
+
+unsigned long calculateHalfPeriod(unsigned long period)
+{
+	return (period/2);
+}
+
---
+++ Zadatak1/main.c
@@ -0,0 +1,30 @@
+/**
+  * @file main.c
+  * @brief Opis datoteke
+  * @author Ime Prezime
+  * @date 11-05-2021
+  * @version 1.0
+  */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+
+ DDRD = 0xff; //port D -> izlaz
+ DDRB |= 1 << 4; //PB4 -> izlaz
+ PORTB &= ~(1 << 4); //PB4 = 0, cime se ukljucuje tranzistor Q0
+
+ while(1)
+ {
+
+ PORTD = 0xaa; //ukljucuju se LED na pozicijama 6, 4, 2 i 0
+ _delay_ms(1000);//pauza 1s
+ PORTD = 0x55; //ukljucuju se LED na pozicijama 7, 5, 3 i 1
+ _delay_ms(1000);//pauza 1s
+
+ }
+ return 0;
+}
+
